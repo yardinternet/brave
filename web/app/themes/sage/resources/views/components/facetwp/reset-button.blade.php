005e56f85d49eef285119cd82912099ee1bffba9
Yannic van Veen <yannic.vanveen@yard.nl>
2026-01-09 13:54:37 +0100

    feat: add custom properties for buttons / refactor button css

--- web/app/themes/sage/resources/views/components/facetwp/reset-button.blade.php
+++ web/app/themes/sage/resources/views/components/facetwp/reset-button.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <button
-	{{ $attributes->merge(['class' => 'js-brave-facetwp-btn-reset is-button is-button-primary-100 group flex w-full items-center justify-center gap-2 lg:w-fit']) }}
+	{{ $attributes->merge(['class' => 'js-brave-facetwp-btn-reset is-button is-button-primary-light group flex w-full items-center justify-center gap-2 lg:w-fit']) }}
 	onclick="FWP.reset()">
 	<i class="fa-regular fa-redo-alt group-hover:rotate-320 transition-all duration-300" aria-hidden="true"></i>
 	{{ $label }}
