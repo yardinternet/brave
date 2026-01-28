77d84702a99b2b916676ee11694e0a019acd7c84
Lara van Diemen <lara@yard.nl>
2026-01-28 14:34:03 +0100

    feat: use new dialog component for facetwp mobile filters, searchbar and mobile menu

--- web/app/themes/sage/resources/views/components/facetwp/mobile-filters.blade.php
+++ web/app/themes/sage/resources/views/components/facetwp/mobile-filters.blade.php
@@ -1,26 +1,28 @@
 @props([
     'facets' => [],
-    'label' => 'Filters',
+    'dialogId' => 'js-brave-facetwp-mobile-filters',
+    'label' => __('Filters', 'sage'),
 ])
 
-<button data-dialog-id="js-facetwp-mobile-filters-dialog" class="is-button z-50 w-full text-center lg:hidden">
+<x-brave::dialog.trigger :dialogId="$dialogId" class="is-button z-50 w-full text-center lg:hidden">
 	<i class="fa-light fa-sliders mr-2" aria-hidden="true"></i>
 	{{ $label }}
-</button>
+</x-brave::dialog.trigger>
 
-<dialog id="js-facetwp-mobile-filters-dialog" aria-label="{{ $label }}" @class([
+<x-brave-dialog :id="$dialogId" :ariaLabel="$label" @class([
 	'transition-discrete max-h-3/4 mt-auto w-full max-w-full translate-y-full rounded-t-lg bg-white opacity-0 transition-all duration-300',
 	'backdrop:transition-discrete backdrop:pointer-events-none backdrop:bg-black/0 backdrop:transition-all backdrop:duration-300',
 	'starting:open:translate-y-full starting:open:opacity-0 starting:open:backdrop:bg-black/0',
 	'open:translate-y-0 open:opacity-100 open:backdrop:bg-black/50',
 ])>
 	<div class="flex h-full flex-col">
-		<div class="z-1 sticky top-0 flex justify-between gap-2 border-b border-gray-100 bg-white p-4">
+		<div class="z-1 sticky top-0 flex items-center justify-between gap-2 border-b border-gray-100 bg-white p-4">
 			<h2 class="mb-0 text-base font-normal">{{ $label }}</h2>
-			<button class="js-dialog-close-button leading-0">
+			<x-brave::dialog.trigger :dialogId="$dialogId"
+				class="leading-0 -m-2 flex size-[46px] items-center justify-center text-2xl">
 				<i class="fa-light fa-xmark" aria-hidden="true"></i>
-				<span class="sr-only">Sluit</span>
-			</button>
+				<span class="sr-only">{{ __('Sluit filters', 'sage') }}</span>
+			</x-brave::dialog.trigger>
 		</div>
 
 		<div class="p-4">
@@ -30,9 +32,9 @@
 		<div class="sticky bottom-4 mt-auto flex gap-2 px-4">
 			<x-facetwp.reset-button />
 
-			<button class="js-dialog-close-button is-button w-full text-center">
-				Bekijk resultaten
-			</button>
+			<x-brave::dialog.trigger :dialogId="$dialogId" class="is-button w-full text-center">
+				{{ __('Bekijk resultaten', 'sage') }}
+			</x-brave::dialog.trigger>
 		</div>
 	</div>
-</dialog>
+</x-brave-dialog>
--- web/app/themes/sage/resources/views/partials/header/mobile-menu.blade.php
+++ web/app/themes/sage/resources/views/partials/header/mobile-menu.blade.php
@@ -1,13 +1,41 @@
-<div id="js-brave-mobile-menu"
-	class="mobile-menu ease-base invisible fixed bottom-0 left-0 right-0 top-[calc(var(--nav-bar-height)+var(--wp-admin-bar-height))] z-40 w-full overflow-y-scroll bg-white opacity-0 transition-all duration-500">
-	<div class="flex h-full flex-col items-start py-4">
+@php
+	$dialogId = 'js-brave-mobile-menu';
+	$label = __('Menu', 'sage');
+@endphp
+
+<x-brave::dialog.trigger :dialogId="$dialogId" class="hamburger group flex h-[46px] flex-col gap-y-2 lg:hidden">
+	<span
+		class="block h-0.5 w-8 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:w-8 group-aria-expanded:translate-y-3 group-aria-expanded:rotate-45"></span>
+	<span
+		class="block h-0.5 w-6 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:scale-0"></span>
+	<span
+		class="block h-0.5 w-4 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:w-8 group-aria-expanded:-translate-y-2 group-aria-expanded:-rotate-45"></span>
+	<span class="mt-auto text-xs leading-none">{{ $label }}</span>
+</x-brave::dialog.trigger>
+
+<x-brave-dialog :id="$dialogId" :ariaLabel="$label" @class([
+	'mobile-menu transition-discrete top-0 bottom-0 left-10 right-0 h-full max-h-full w-auto max-w-full translate-x-full bg-white opacity-0 transition-all duration-500',
+	'backdrop:transition-discrete backdrop:pointer-events-none backdrop:bg-black/0 backdrop:transition-all backdrop:duration-500',
+	'starting:open:translate-x-full starting:open:opacity-0 starting:open:backdrop:bg-black/0',
+	'open:translate-x-0 open:opacity-100 open:backdrop:bg-black/50',
+])>
+	<div class="flex h-full flex-col">
+		<div class="z-1 sticky top-0 flex items-center justify-between gap-2 border-b border-gray-100 bg-white p-4">
+			<h2 class="mb-0 text-base font-normal">{{ $label }}</h2>
+			<x-brave::dialog.trigger :dialogId="$dialogId"
+				class="leading-0 -m-2 flex size-[46px] items-center justify-center text-2xl">
+				<i class="fa-light fa-xmark" aria-hidden="true"></i>
+				<span class="sr-only">Sluit menu</span>
+			</x-brave::dialog.trigger>
+		</div>
+
 		@php
 			if (has_nav_menu('primary_navigation')) {
 			    wp_nav_menu([
 			        'container' => '',
 			        'depth' => 2,
 			        'id' => '',
-			        'menu_class' => 'mobile-menu-navigation | w-full mb-0 list-none px-4',
+			        'menu_class' => 'mobile-menu-navigation w-full mb-0 list-none px-4',
 			        'theme_location' => 'primary_navigation',
 			    ]);
 			}
@@ -17,12 +45,10 @@
 			        'container' => '',
 			        'depth' => 1,
 			        'id' => '',
-			        'menu_class' => 'mobile-menu-navigation mobile-menu-top-bar | w-full mt-6 mb-0 list-none px-4',
+			        'menu_class' => 'mobile-menu-navigation mobile-menu-top-bar w-full mb-0 list-none px-4',
 			        'theme_location' => 'top_bar_navigation',
 			    ]);
 			}
 		@endphp
-		<button id="js-brave-mobile-menu-close-btn" class="is-button on-focus-visible focus:!static">{{ __('Sluiten', 'sage') }}
-		</button>
 	</div>
-</div>
+</x-brave-dialog>
