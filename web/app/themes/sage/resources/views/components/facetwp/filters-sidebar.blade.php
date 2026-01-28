1a4c029fb38f30c930bf0be653b2e30f5346ba57
Lara van Diemen <lara@yard.nl>
2026-01-28 09:06:52 +0100

    feat: create facetwp filters dialog

--- web/app/themes/sage/resources/views/components/facetwp/filters-sidebar.blade.php
+++ web/app/themes/sage/resources/views/components/facetwp/filters-sidebar.blade.php
@@ -3,33 +3,15 @@
 ])
 
 @if (!empty($facets))
-	<h2 class="sr-only">Filters</h2>
-
 	<a class="is-button on-focus-visible" href="#js-brave-facetwp-template-view">
 		Sla filters over
 	</a>
 
-	<x-brave-accordion>
-		<x-brave::accordion.item>
-			<x-brave::accordion.item.trigger class="is-button w-full lg:hidden">
-				<i class="fa-light fa-sliders mr-2" aria-hidden="true"></i>
-				Filters
-			</x-brave::accordion.item.trigger>
+	<x-facetwp.mobile-filters :facets="$facets" />
 
-			<x-brave::accordion.item.panel class="lg:!block lg:!h-auto lg:overflow-visible">
-				<div class="mt-3 lg:mt-0">
-					@foreach ($facets as $facet)
-						<fieldset>
-							<legend class="mb-2">
-								<h3 class="mb-0">{{ $facet['label'] }}</h3>
-							</legend>
-							{!! facetwp_display('facet', $facet['name']) !!}
-						</fieldset>
-					@endforeach
-
-					<x-facetwp.reset-button />
-				</div>
-			</x-brave::accordion.item.panel>
-		</x-brave::accordion.item>
-	</x-brave-accordion>
+	<div class="hidden lg:block">
+		<h2 class="sr-only">Filters</h2>
+		<x-facetwp.filters :facets="$facets" />
+		<x-facetwp.reset-button />
+	</div>
 @endif
