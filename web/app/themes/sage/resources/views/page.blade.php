1b27a954cbcf80ce5e80f29f822a8ec14aa69b01
YvetteNikolov <yvettenikolov@gmail.com>
2026-01-29 17:57:52 +0100

    refactor: is-button-primary-light => is-button-subtle

--- web/app/themes/sage/resources/views/page.blade.php
+++ web/app/themes/sage/resources/views/page.blade.php
@@ -1,4 +1,19 @@
 <x-layout>
+	<div class="wp-block-buttons">
+		<a class="is-button" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
+	</div>
+	<div class="wp-block-buttons">
+		<a class="is-button" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
+	</div>
+	<div class="wp-block-buttons">
+		<a class="is-button" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
+		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
+	</div>
 	@while (have_posts())
 		@php(the_post())
 		@php(the_content())
