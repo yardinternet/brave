938f984778f42e4a0e8ce45ed68a78b4e80ea8b2
YvetteNikolov <yvettenikolov@gmail.com>
2026-01-29 19:43:15 +0100

    feat: add card variables for use in reactive-search

--- web/app/themes/sage/resources/views/page.blade.php
+++ web/app/themes/sage/resources/views/page.blade.php
@@ -1,19 +1,4 @@
 <x-layout>
-	<div class="wp-block-buttons">
-		<a class="is-button" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
-	</div>
-	<div class="wp-block-buttons">
-		<a class="is-button" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
-	</div>
-	<div class="wp-block-buttons">
-		<a class="is-button" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-subtle" href="https://brave.local/">Test knop</a>
-		<a class="is-button is-button-outline" href="https://brave.local/">Test knop</a>
-	</div>
 	@while (have_posts())
 		@php(the_post())
 		@php(the_content())
