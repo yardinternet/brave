0019ec62f4f386fbca5dcdaa4590d936309782c0
YvetteNikolov <yvettenikolov@gmail.com>
2026-02-14 11:47:00 +0100

    feat: implement log1x crumb package

---
+++ web/app/themes/sage/config/breadcrumb.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the label used when displaying the homepage on the
+	| breadcrumb.
+	|
+	*/
+
+	'home' => __('Home', 'sage'),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Blog
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the label used when displaying the posts page on the
+	| breadcrumb. If a posts page is not configured, this option can be
+	| disregarded.
+	|
+	| If a posts page is configured but you would not like it shown in the
+	| breadcrumb, you may explicitly set this option to `false`.
+	|
+	*/
+
+	'blog' => get_the_title(
+		get_option('page_for_posts')
+	),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Author
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the label used when displaying the author archive
+	| page on the breadcrumb.
+	|
+	| The `%s` identifier represents the authors configured display name.
+	|
+	*/
+
+	'author' => __('Posts by %s', 'sage'),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Search
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the label used when displaying the search results
+	| page on the breadcrumb.
+	|
+	| The `%s` identifier represents the current search query.
+	|
+	*/
+
+	'search' => __('Zoekresultaten', 'sage'),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Not Found
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the label used when displaying the 404 error page
+	| on the breadcrumb.
+	|
+	*/
+
+	'not_found' => __('404 error', 'sage'),
+];
--- web/app/themes/sage/resources/views/components/breadcrumbs.blade.php
+++
@@ -1,5 +0,0 @@
-@if (!is_front_page() && function_exists('seopress_display_breadcrumbs'))
-	<div class="alignwide wp-block-wpseopress-breadcrumbs relative my-3">
-		{!! seopress_display_breadcrumbs() !!}
-	</div>
-@endif
