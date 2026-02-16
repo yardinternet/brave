dd5e55ad5f4301e2fbdcde3364954f95efbc3968
Yannic van Veen <yannic.vanveen@yard.nl>
2026-02-16 14:41:53 +0100

    feat: add styles for brave components breadcrumbs component

--- web/app/themes/sage/resources/views/components/breadcrumbs.blade.php
+++ web/app/themes/sage/resources/views/components/breadcrumbs.blade.php
@@ -1,5 +1,27 @@
-@if (!is_front_page() && function_exists('seopress_display_breadcrumbs'))
-	<div class="alignwide wp-block-wpseopress-breadcrumbs relative my-3">
-		{!! seopress_display_breadcrumbs() !!}
-	</div>
+@php
+	$listClass = 'flex flex-wrap items-center list-none pl-0 mb-0 gap-x-(--breadcrumbs-list-gap-x) gap-y-(--breadcrumbs-list-gap-y)';
+
+	$itemClass = implode(' ', [
+		'flex items-center gap-x-(--breadcrumbs-item-gap-x)',
+		'first:before:fontawesome first:before:text-(length:--breadcrumbs-icon-font-size) first:before:content-(--breadcrumbs-home-icon)',
+		'not-last:after:fontawesome not-last:after:content-(--breadcrumbs-separator-icon) not-last:after:text-(length:--breadcrumbs-icon-font-size)',
+	]);
+
+	$linkClass = 'text-current no-underline hover:text-current hover:underline';
+@endphp
+
+@if (!is_front_page())
+	<nav class="py-(--breadcrumbs-padding-y) leading-(--breadcrumbs-leading) text-(length:--breadcrumbs-font-size)">
+		<ul @class([$listClass])>
+			<li @class([$itemClass])>
+				<a @class([$linkClass]) href="/">Home</a>
+			</li>
+			<li @class([$itemClass])>
+				<a @class([$linkClass]) href="/projecten">Projecten</a>
+			</li>
+			<li @class([$itemClass])>
+				<span>Pariatur sint sunt velit</span>
+			</li>
+		</ul>
+	</nav>
 @endif
