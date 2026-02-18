88dfd262dc44639b92f49176a7c40607d88b49f7
Yannic van Veen <yannic.vanveen@yard.nl>
2026-02-18 14:48:11 +0100

    feat: use gap for primary nav list

--- web/app/themes/sage/resources/views/components/header/navigation.blade.php
+++ web/app/themes/sage/resources/views/components/header/navigation.blade.php
@@ -6,17 +6,18 @@
 
 @if ($primaryNavigation->isNotEmpty())
 	<nav class="nav-primary hidden items-center lg:flex" aria-label="{{ __('Primaire navigatie', 'sage') }}">
-		<ul class="nav flex h-full items-center justify-center">
-			@foreach ($menu->all() as $item)
+		<ul class="nav list-reset flex h-full items-center justify-center gap-x-4 xl:gap-x-8">
+			@foreach ($primaryNavigation->all() as $item)
 				<li @class([
 					'menu-item group relative h-full',
 					'menu-item-has-children' => $item->children,
 				])>
 					<a href="{{ $item->url }}" @class([
-						'relative flex h-full gap-2 items-center px-2 text-center text-sm leading-snug text-black no-underline xl:px-4 xl:text-base xl:leading-snug hocus:text-primary',
+						'relative flex h-full gap-2 items-center text-center text-sm leading-snug text-black no-underline xl:text-base xl:leading-snug hocus:text-primary',
 						'text-primary' => $item->active || $item->activeParent,
 						$item->classes,
-					]) @if($item->active) aria-current="page" @endif>
+					])
+						@if ($item->active) aria-current="page" @endif>
 						{{ $item->label }}
 
 						@if ($item->children)
@@ -25,7 +26,7 @@
 					</a>
 
 					<span @class([
-						'bg-primary ease-base absolute bottom-0 left-2 h-1 w-[calc(100%-1rem)] duration-300 xl:left-4 xl:w-[calc(100%-2rem)] invisible scale-0 group-hocus:visible group-hocus:scale-100',
+						'bg-primary ease-base absolute bottom-0 w-full left-0 h-1 duration-300 invisible scale-0 group-hover:visible group-hover:scale-100',
 						'visible scale-100' => $item->active || $item->activeParent,
 					]) aria-hidden="true"></span>
 
@@ -37,7 +38,8 @@
 									<a href="{{ $child->url }}" @class([
 										'group/sub-link flex items-center gap-6 justify-between px-6 py-3 text-left leading-snug text-inherit no-underline',
 										'text-primary' => $child->active,
-									]) @if($child->active) aria-current="page" @endif>
+									])
+										@if ($child->active) aria-current="page" @endif>
 										{{ $child->label }}
 										<i class="fa-light fa-angle-right transition-all group-hover/sub-link:translate-x-1"></i>
 									</a>
