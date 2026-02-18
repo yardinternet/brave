9744c47b5f2b480ed84b4c1e9b9d3d0969b915a7
Yannic van Veen <yannic.vanveen@yard.nl>
2026-02-18 11:55:13 +0100

    feat: clearer group name / replace padding with gap / move list reset classes to util

--- web/app/themes/sage/resources/views/components/header/navigation.blade.php
+++ web/app/themes/sage/resources/views/components/header/navigation.blade.php
@@ -6,21 +6,21 @@
 
 @if ($menu->isNotEmpty())
 	<nav class="nav-primary hidden items-center lg:flex" aria-label="{{ __('Primaire navigatie', 'sage') }}">
-		<ul class="nav mb-0 flex h-full list-none items-center justify-center pl-0">
+		<ul class="nav flex h-full list-reset items-center justify-center">
 			@foreach ($menu->all() as $item)
 				<li @class([
 					'menu-item group relative h-full',
 					'menu-item-has-children' => $item->children,
 				])>
 					<a href="{{ $item->url }}" @class([
-						'relative flex h-full items-center px-2 text-center text-sm leading-snug text-black no-underline xl:px-4 xl:text-base xl:leading-snug hocus:text-primary',
+						'relative flex h-full gap-2 items-center px-2 text-center text-sm leading-snug text-black no-underline xl:px-4 xl:text-base xl:leading-snug hocus:text-primary',
 						'text-primary' => $item->active || $item->activeParent,
 						$item->classes,
 					])>
 						{{ $item->label }}
 
 						@if ($item->children)
-							<i class="fa-light fa-chevron-down pl-2"></i>
+							<i class="fa-light fa-chevron-down"></i>
 						@endif
 					</a>
 
@@ -31,15 +31,15 @@
 
 					@if ($item->children)
 						<ul
-							class="sub-menu ease-base invisible absolute mb-0 min-w-48 -translate-y-3 list-none bg-white pl-0 opacity-0 shadow-md transition-all group-has-aria-expanded:visible group-has-aria-expanded:translate-y-0 group-has-aria-expanded:opacity-100">
+							class="sub-menu list-reset ease-base invisible absolute min-w-48 -translate-y-3 bg-white opacity-0 shadow-md transition-all group-has-aria-expanded:visible group-has-aria-expanded:translate-y-0 group-has-aria-expanded:opacity-100">
 							@foreach ($item->children as $child)
 								<li class="menu-item">
 									<a href="{{ $child->url }}" @class([
-										'group/child flex items-center px-6 py-3 text-left leading-snug text-inherit no-underline',
+										'group/sub-link flex items-center gap-6 justify-between px-6 py-3 text-left leading-snug text-inherit no-underline',
 										'text-primary' => $child->active,
 									])>
 										{{ $child->label }}
-										<i class="fa-light fa-angle-right ml-auto pl-6 transition-all group-hover/child:translate-x-1"></i>
+										<i class="fa-light fa-angle-right transition-all group-hover/sub-link:translate-x-1"></i>
 									</a>
 								</li>
 							@endforeach
