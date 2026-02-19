a6ebe57dd1f1b235cc0ed894803f301f49adf019
YvetteNikolov <yvettenikolov@gmail.com>
2026-02-19 13:50:34 +0100

    chore: update dependencies and run formatting and linting

--- web/app/themes/sage/resources/views/404.blade.php
+++ web/app/themes/sage/resources/views/404.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 	<div class="container my-10 md:my-16">
-		<div class="p-(--card-padding) normalize-child-margin mx-auto rounded-theme bg-white shadow md:w-3/4 lg:w-1/2">
+		<div class="p-(--card-padding) normalize-child-margin rounded-theme mx-auto bg-white shadow md:w-3/4 lg:w-1/2">
 			<x-brave-pattern-content slug="404" />
 		</div>
 	</div>
