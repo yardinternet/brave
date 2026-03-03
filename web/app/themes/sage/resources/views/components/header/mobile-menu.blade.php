b947ea5e641223b3147f921ee204a5d84269b2e2
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-03 14:44:17 +0100

    refactor: move mobile-menu.css into blade template

--- web/app/themes/sage/resources/views/components/header/mobile-menu.blade.php
+++ web/app/themes/sage/resources/views/components/header/mobile-menu.blade.php
@@ -12,7 +12,7 @@
 	use Log1x\Navi\Navi;
 @endphp
 
-<x-brave::dialog.trigger :dialogId="$dialogId" class="hamburger group flex h-11.5 flex-col gap-y-2 lg:hidden">
+<x-brave::dialog.trigger :dialogId="$dialogId" class="hamburger h-11.5 group flex flex-col gap-y-2 lg:hidden">
 	<span
 		class="block h-0.5 w-8 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:w-8 group-aria-expanded:translate-y-3 group-aria-expanded:rotate-45"></span>
 	<span
@@ -31,31 +31,39 @@
 	<div class="flex h-full flex-col">
 		<div class="z-1 sticky top-0 flex items-center justify-between gap-2 border-b border-gray-100 bg-white p-4">
 			<h2 class="mb-0 text-base font-normal">{{ $label }}</h2>
-			<x-brave::dialog.trigger :dialogId="$dialogId"
-				class="leading-0 -m-2 flex size-11.5 items-center justify-center text-2xl">
+			<x-brave::dialog.trigger :dialogId="$dialogId" class="leading-0 size-11.5 -m-2 flex items-center justify-center text-2xl">
 				<i class="fa-light fa-xmark" aria-hidden="true"></i>
 				<span class="sr-only">Sluit menu</span>
 			</x-brave::dialog.trigger>
 		</div>
 
-		@if($primaryNavigation->isNotEmpty())
-			<ul class="mobile-menu-navigation list-reset px-4">
-				@foreach($primaryNavigation->all() as $item)
+		@if ($primaryNavigation->isNotEmpty())
+			<ul class="mobile-menu-navigation list-reset mb-6 px-4">
+				@foreach ($primaryNavigation->all() as $item)
 					@php
 						$hasChildren = !empty($item->children);
 					@endphp
+
 					<li @class([
-						'menu-item',
-						'menu-item-has-children' => $hasChildren,
+						'menu-item group',
+						{{-- menu-item-has-children => Now a trigger for MobileMenu. Reset to brave-menu-item-has-children --}}
+						"menu-item-has-children" => $hasChildren,
 					])>
-						<a href="{{ esc_url($item->url) }}"
-							@if($item->active) aria-current="page" @endif>{{ $item->label }}</a>
-						@if($hasChildren)
-							<ul class="sub-menu list-reset">
-								@foreach($item->children as $item)
+						<a @class([
+							'block py-3 text-base text-black no-underline focus:text-inherit',
+							'text-primary font-bold' => $item->active || $item->activeParent,
+							"after:fontawesome after:inline-flex after:px-2 after:transition-all after:content-['\\f107']! group-has-aria-expanded:after:rotate-180" => $hasChildren,
+						]) href="{{ $hasChildren ? '#' : esc_url($item->url) }}"
+							@if ($item->active) aria-current="page" @endif>{{ $item->label }}</a>
+						@if ($hasChildren)
+							<ul class="sub-menu list-reset mb-2 hidden list-none px-3 group-has-aria-expanded:block!">
+								@foreach ($item->children as $child)
 									<li class="menu-item">
-										<a href="{{ esc_url($item->url) }}"
-											@if($item->active) aria-current="page" @endif>{{ $item->label }}</a>
+										<a @class([
+											'block py-1 text-sm text-gray-700 no-underline',
+											'text-primary' => $child->active,
+										]) href="{{ esc_url($child->url) }}"
+											@if ($child->active) aria-current="page" @endif>{{ $child->label }}</a>
 									</li>
 								@endforeach
 							</ul>
@@ -65,13 +73,13 @@
 			</ul>
 		@endif
 
-		@if($topBarNavigation->isNotEmpty())
-			<ul class="grid gap-2 list-reset px-4 text-sm text-gray-700">
-				@foreach($topBarNavigation->all() as $item)
+		@if ($topBarNavigation->isNotEmpty())
+			<ul class="list-reset grid gap-2 px-4 text-sm text-gray-700">
+				@foreach ($topBarNavigation->all() as $item)
 					<li>
-						<a class="no-underline aria-current-page:underline text-current hover:text-current"
+						<a class="aria-current-page:underline text-current no-underline hover:text-current"
 							href="{{ esc_url($item->url) }}"
-							@if($item->active) aria-current="page" @endif>{{ $item->label }}</a>
+							@if ($item->active) aria-current="page" @endif>{{ $item->label }}</a>
 					</li>
 				@endforeach
 			</ul>
