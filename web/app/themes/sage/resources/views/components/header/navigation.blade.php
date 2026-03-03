7ba1b0bba0266fa6977ccfe7bd366d86ca09720c
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-03 15:01:54 +0100

    style: small styling changes

--- web/app/themes/sage/resources/views/components/header/navigation.blade.php
+++ web/app/themes/sage/resources/views/components/header/navigation.blade.php
@@ -6,7 +6,7 @@
 
 @if ($primaryNavigation->isNotEmpty())
 	<nav class="nav-primary hidden items-center lg:flex" aria-label="{{ __('Primaire navigatie', 'sage') }}">
-		<ul class="nav flex gap-x-4 xl:gap-x-8 h-full list-reset items-center justify-center">
+		<ul class="nav list-reset flex h-full items-center justify-center gap-x-4 xl:gap-x-8">
 			@foreach ($primaryNavigation->all() as $item)
 				<li @class([
 					'menu-item group relative h-full',
@@ -16,7 +16,8 @@
 						'relative flex h-full gap-2 items-center text-center text-sm leading-snug text-black no-underline xl:text-base xl:leading-snug hocus:text-primary',
 						'text-primary' => $item->active || $item->activeParent,
 						$item->classes,
-					]) @if($item->active) aria-current="page" @endif>
+					])
+						@if ($item->active) aria-current="page" @endif>
 						{{ $item->label }}
 
 						@if ($item->children)
@@ -31,13 +32,14 @@
 
 					@if ($item->children)
 						<ul
-							class="sub-menu list-reset ease-base invisible absolute min-w-48 -translate-y-3 bg-white opacity-0 shadow-md transition-all group-has-aria-expanded:visible group-has-aria-expanded:translate-y-0 group-has-aria-expanded:opacity-100">
+							class="sub-menu list-reset ease-base group-has-aria-expanded:visible group-has-aria-expanded:translate-y-0 group-has-aria-expanded:opacity-100 invisible absolute min-w-48 -translate-y-3 bg-white opacity-0 shadow-md transition-all">
 							@foreach ($item->children as $child)
 								<li class="menu-item">
 									<a href="{{ $child->url }}" @class([
 										'group/sub-link flex items-center gap-6 justify-between px-6 py-3 text-left leading-snug text-inherit no-underline',
 										'text-primary' => $child->active,
-									]) @if($child->active) aria-current="page" @endif>
+									])
+										@if ($child->active) aria-current="page" @endif>
 										{{ $child->label }}
 										<i class="fa-light fa-angle-right transition-all group-hover/sub-link:translate-x-1"></i>
 									</a>
--- web/app/themes/sage/resources/views/components/header/top-bar.blade.php
+++ web/app/themes/sage/resources/views/components/header/top-bar.blade.php
@@ -5,16 +5,19 @@
 @endphp
 
 @if ($topBarNavigation->isNotEmpty())
-	<div class="top-bar h-(--top-bar-height) hidden bg-gray-100 lg:flex text-sm">
+	<div class="top-bar h-(--top-bar-height) hidden bg-gray-100 text-sm lg:flex">
 		<div class="container h-full">
-			<nav class="flex h-full items-center justify-end"
-				aria-label="{{ __('Secundaire navigatie', 'sage') }}">
-				<ul class="flex items-center gap-4 list-reset">
-					@foreach($topBarNavigation->all() as $item)
+			<nav class="flex h-full items-center justify-end" aria-label="{{ __('Secundaire navigatie', 'sage') }}">
+				<ul class="list-reset flex items-center gap-4">
+					@foreach ($topBarNavigation->all() as $item)
 						<li>
-							<a class="text-current no-underline hover:text-current hover:underline aria-current-page:underline"
-								href="{{ esc_url($item->url) }}"
-								@if($item->active) aria-current="page" @endif>{{ $item->label }}</a>
+							<a @class([
+								'text-current no-underline hover:text-current hover:underline',
+								'text-primary' => $item->active,
+							]) href="{{ esc_url($item->url) }}"
+								@if ($item->active) aria-current="page" @endif>
+								{{ $item->label }}
+							</a>
 						</li>
 					@endforeach
 				</ul>
