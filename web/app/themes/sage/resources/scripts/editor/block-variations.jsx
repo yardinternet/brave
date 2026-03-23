2d9c4a55f2f750ea6f8c8b7b71fc8a8a8ac7ff47
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-23 18:00:30 +0100

    feat: only register certain block variations when on the page post type

--- web/app/themes/sage/resources/scripts/editor/block-variations.jsx
+++ web/app/themes/sage/resources/scripts/editor/block-variations.jsx
@@ -33,140 +33,151 @@ window.addEventListener( 'DOMContentLoaded', () => {
 		},
 	} );
 
-	// Group: card
-	registerBlockVariation( 'core/group', {
-		isActive: [ 'className' ],
-		name: 'wp-block-group-card',
-		title: 'Kaart',
-		description: 'Een kaart met een koptekst en inleiding.',
-		attributes: {
-			className: 'is-variation-card',
-			backgroundColor: 'white',
-		},
-		icon: {
-			src: (
-				<svg viewBox="0 0 640 640" xmlns="http://www.w3.org/2000/svg">
-					<path d="m518.7 128.3c32.3 3.3 57.5 30.5 57.5 63.7v320l-.3 6.5c-3.1 30.1-27 54.1-57.1 57.1l-6.5.3h-192l-6.5-.3c-12.1-1.2-23.1-5.9-32.3-12.8l70.3-18.8h160.5c17.7 0 32-14.3 32-32v-320c0-17.7-14.3-32-32-32h-75l-8.6-32h83.5zm-221.4-82.9c32-5.2 63.4 14.6 72 46.6l82.8 309.1 1.4 6.4c4.8 29.9-12.1 59.2-40.4 70l-6.2 2-185.4 49.7-6.4 1.3c-29.9 4.8-59.2-12.1-70-40.4l-2.1-6.1-82.8-309.1c-8.6-32 8.7-64.9 39-76.4l6.2-2 185.5-49.7 6.4-1.3zm41.1 54.9c-4.6-17.1-22.1-27.2-39.2-22.6l-185.5 49.7c-17 4.6-27.2 22.1-22.6 39.2l82.8 309.1c4.6 17.1 22.1 27.2 39.2 22.6l185.4-49.7c17.1-4.6 27.2-22.1 22.6-39.2z" />
-				</svg>
-			),
-			foreground: BlockIconColor.foreground,
-		},
-		scope: [ 'block', 'inserter' ],
-		innerBlocks: [
-			[ 'core/heading', { placeholder: 'Koptekst h3', level: 3 } ],
-			[
-				'core/paragraph',
-				{
-					placeholder:
-						'Korte inleiding van maximaal 5 regels. Plaats een link achter de koptekst voor een volledig klikbare kaart.',
-				},
-			],
-		],
-	} );
-
-	// Group: layout article
-	registerBlockVariation( 'core/group', {
-		name: 'wp-block-group-layout-article',
-		title: 'Layout: artikel',
-		description: 'Een layout met witte achtergrond en vaste breedte.',
-		attributes: {
-			className: 'layout-article',
-			tagName: 'article',
-		},
-		isActive: [ 'className' ],
-		icon: {
-			src: (
-				<svg viewBox="0 0 640 640" xmlns="http://www.w3.org/2000/svg">
-					<path d="m192 96h128v96c0 35.3 28.7 64 64 64h96v256c0 17.7-14.3 32-32 32h-256c-17.7 0-32-14.3-32-32v-384c0-17.7 14.3-32 32-32zm160 13.3 114.7 114.7h-82.7c-17.7 0-32-14.3-32-32zm-160-45.3c-35.3 0-64 28.7-64 64v384c0 35.3 28.7 64 64 64h256c35.3 0 64-28.7 64-64v-261.5c0-17-6.7-33.3-18.7-45.3l-122.6-122.5c-12-12-28.2-18.7-45.2-18.7zm48 256c-8.8 0-16 7.2-16 16s7.2 16 16 16h160c8.8 0 16-7.2 16-16s-7.2-16-16-16zm0 96c-8.8 0-16 7.2-16 16s7.2 16 16 16h160c8.8 0 16-7.2 16-16s-7.2-16-16-16z" />
-				</svg>
-			),
-			foreground: BlockIconColor.foreground,
-		},
-		scope: [ 'block', 'inserter' ],
-		innerBlocks: [
-			[ 'theme/back-button' ],
-			[ 'core/post-title', { level: 1 } ],
-			[ 'core/paragraph', { content: 'Voeg de inhoud toe' } ],
-		],
-	} );
-
-	// Columns: article aside layout variation
-	registerBlockVariation( 'core/columns', {
-		name: 'wp-block-columns-layout-article-aside',
-		title: 'Layout: artikel met zijbalk',
-		description: 'Een layout met een hoofdartikel en een zijbalk.',
-		attributes: {
-			align: 'wide',
-			className: 'layout-article-aside',
-			verticalAlignment: 'top',
-		},
-		scope: [ 'block', 'inserter' ],
-		icon: {
-			src: (
-				<svg xmlns="http://www.w3.org/2000/svg">
-					<path
-						fillRule="evenodd"
-						d="M15 7.5h-5v10h5v-10Zm1.5 0v10H19a.5.5 0 0 0 .5-.5V8a.5.5 0 0 0-.5-.5h-2.5ZM6 7.5h2.5v10H6a.5.5 0 0 1-.5-.5V8a.5.5 0 0 1 .5-.5ZM6 6h13a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2Z"
-						clipRule="evenodd"
-					/>
-				</svg>
-			),
-			foreground: BlockIconColor.foreground,
-		},
-		isActive: [ 'className' ],
-		innerBlocks: [
-			[
-				'core/column',
-				{
-					width: '66.66%',
-					lock: { move: true, remove: true },
-					className: 'layout-article-aside__article',
-				},
+	/**
+	 * Only register these variation on the page post type
+	 */
+	if ( window.theme?.currentPostType === 'page' ) {
+		// Group: card
+		registerBlockVariation( 'core/group', {
+			isActive: [ 'className' ],
+			name: 'wp-block-group-card',
+			title: 'Kaart',
+			description: 'Een kaart met een koptekst en inleiding.',
+			attributes: {
+				className: 'is-variation-card',
+				backgroundColor: 'white',
+			},
+			icon: {
+				src: (
+					<svg
+						viewBox="0 0 640 640"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path d="m518.7 128.3c32.3 3.3 57.5 30.5 57.5 63.7v320l-.3 6.5c-3.1 30.1-27 54.1-57.1 57.1l-6.5.3h-192l-6.5-.3c-12.1-1.2-23.1-5.9-32.3-12.8l70.3-18.8h160.5c17.7 0 32-14.3 32-32v-320c0-17.7-14.3-32-32-32h-75l-8.6-32h83.5zm-221.4-82.9c32-5.2 63.4 14.6 72 46.6l82.8 309.1 1.4 6.4c4.8 29.9-12.1 59.2-40.4 70l-6.2 2-185.4 49.7-6.4 1.3c-29.9 4.8-59.2-12.1-70-40.4l-2.1-6.1-82.8-309.1c-8.6-32 8.7-64.9 39-76.4l6.2-2 185.5-49.7 6.4-1.3zm41.1 54.9c-4.6-17.1-22.1-27.2-39.2-22.6l-185.5 49.7c-17 4.6-27.2 22.1-22.6 39.2l82.8 309.1c4.6 17.1 22.1 27.2 39.2 22.6l185.4-49.7c17.1-4.6 27.2-22.1 22.6-39.2z" />
+					</svg>
+				),
+				foreground: BlockIconColor.foreground,
+			},
+			scope: [ 'block', 'inserter' ],
+			innerBlocks: [
+				[ 'core/heading', { placeholder: 'Koptekst h3', level: 3 } ],
 				[
-					[ 'core/post-title', { level: 1 } ],
-					[ 'core/paragraph', { content: 'Voeg de inhoud toe' } ],
+					'core/paragraph',
+					{
+						placeholder:
+							'Korte inleiding van maximaal 5 regels. Plaats een link achter de koptekst voor een volledig klikbare kaart.',
+					},
 				],
 			],
-			[
-				'core/column',
-				{
-					width: '33.33%',
-					lock: { move: true, remove: true },
-					className: 'layout-article-aside__aside',
-					layout: { type: 'default' },
-				},
+		} );
+
+		// Group: layout article
+		registerBlockVariation( 'core/group', {
+			name: 'wp-block-group-layout-article',
+			title: 'Layout: artikel',
+			description: 'Een layout met witte achtergrond en vaste breedte.',
+			attributes: {
+				className: 'layout-article',
+				tagName: 'article',
+			},
+			isActive: [ 'className' ],
+			icon: {
+				src: (
+					<svg
+						viewBox="0 0 640 640"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path d="m192 96h128v96c0 35.3 28.7 64 64 64h96v256c0 17.7-14.3 32-32 32h-256c-17.7 0-32-14.3-32-32v-384c0-17.7 14.3-32 32-32zm160 13.3 114.7 114.7h-82.7c-17.7 0-32-14.3-32-32zm-160-45.3c-35.3 0-64 28.7-64 64v384c0 35.3 28.7 64 64 64h256c35.3 0 64-28.7 64-64v-261.5c0-17-6.7-33.3-18.7-45.3l-122.6-122.5c-12-12-28.2-18.7-45.2-18.7zm48 256c-8.8 0-16 7.2-16 16s7.2 16 16 16h160c8.8 0 16-7.2 16-16s-7.2-16-16-16zm0 96c-8.8 0-16 7.2-16 16s7.2 16 16 16h160c8.8 0 16-7.2 16-16s-7.2-16-16-16z" />
+					</svg>
+				),
+				foreground: BlockIconColor.foreground,
+			},
+			scope: [ 'block', 'inserter' ],
+			innerBlocks: [
+				[ 'theme/back-button' ],
+				[ 'core/post-title', { level: 1 } ],
+				[ 'core/paragraph', { content: 'Voeg de inhoud toe' } ],
+			],
+		} );
+
+		// Columns: article aside layout variation
+		registerBlockVariation( 'core/columns', {
+			name: 'wp-block-columns-layout-article-aside',
+			title: 'Layout: artikel met zijbalk',
+			description: 'Een layout met een hoofdartikel en een zijbalk.',
+			attributes: {
+				align: 'wide',
+				className: 'layout-article-aside',
+				verticalAlignment: 'top',
+			},
+			scope: [ 'block', 'inserter' ],
+			icon: {
+				src: (
+					<svg xmlns="http://www.w3.org/2000/svg">
+						<path
+							fillRule="evenodd"
+							d="M15 7.5h-5v10h5v-10Zm1.5 0v10H19a.5.5 0 0 0 .5-.5V8a.5.5 0 0 0-.5-.5h-2.5ZM6 7.5h2.5v10H6a.5.5 0 0 1-.5-.5V8a.5.5 0 0 1 .5-.5ZM6 6h13a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2Z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				),
+				foreground: BlockIconColor.foreground,
+			},
+			isActive: [ 'className' ],
+			innerBlocks: [
 				[
+					'core/column',
+					{
+						width: '66.66%',
+						lock: { move: true, remove: true },
+						className: 'layout-article-aside__article',
+					},
 					[
-						'core/group',
-						{
-							backgroundColor: 'white',
-							style: {
-								spacing: {
-									margin: {
-										top: '0',
-										bottom: '0',
+						[ 'core/post-title', { level: 1 } ],
+						[ 'core/paragraph', { content: 'Voeg de inhoud toe' } ],
+					],
+				],
+				[
+					'core/column',
+					{
+						width: '33.33%',
+						lock: { move: true, remove: true },
+						className: 'layout-article-aside__aside',
+						layout: { type: 'default' },
+					},
+					[
+						[
+							'core/group',
+							{
+								backgroundColor: 'white',
+								style: {
+									spacing: {
+										margin: {
+											top: '0',
+											bottom: '0',
+										},
 									},
 								},
 							},
-						},
-						[
 							[
-								'core/heading',
-								{ level: 2, content: 'Zie ook' },
-							],
-							[
-								'core/paragraph',
-								{ content: 'Voeg de inhoud toe' },
+								[
+									'core/heading',
+									{ level: 2, content: 'Zie ook' },
+								],
+								[
+									'core/paragraph',
+									{ content: 'Voeg de inhoud toe' },
+								],
 							],
 						],
 					],
 				],
 			],
-		],
-		supports: {
-			multiple: false,
-		},
-	} );
+			supports: {
+				multiple: false,
+			},
+		} );
+	}
 
 	unusedVariations.forEach( ( { block, variation } ) => {
 		unregisterBlockVariation( block, variation );
