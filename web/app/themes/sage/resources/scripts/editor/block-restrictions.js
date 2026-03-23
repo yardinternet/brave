d2a6f51789f543bb9c6839033f2cf72651e20679
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-23 18:12:18 +0100

    feat: refactor block-restriction.js a bit

--- web/app/themes/sage/resources/scripts/editor/block-restrictions.js
+++ web/app/themes/sage/resources/scripts/editor/block-restrictions.js
@@ -3,25 +3,6 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
-const blockSets = window?.theme.gutenbergConfig?.blockSets ?? {};
-const innerBlockRestrictions =
-	window?.theme.gutenbergConfig?.innerBlockRestrictions ?? {};
-
-/**
- * Resolve config for a parent block
- */
-const resolveAllowedBlocks = ( rule = {}, defaultAllowedBlocks = [] ) => {
-	const base = rule.allowedBlocks ?? blockSets[ rule.blockSet ] ?? [];
-	const add = rule.add ?? [];
-	const remove = rule.remove ?? [];
-
-	const merged = [ ...defaultAllowedBlocks, ...base, ...add ];
-
-	return [ ...new Set( merged ) ].filter(
-		( blockName ) => ! remove.includes( blockName )
-	);
-};
-
 /**
  * Only allow certain blocks in some blocks. See `innerBlockRestrictions` in `config/gutenberg.php` for configuration.
  */
@@ -29,6 +10,9 @@ addFilter(
 	'blocks.registerBlockType',
 	'yard/restrict-inner-blocks',
 	( settings, name ) => {
+		const innerBlockRestrictions =
+			window?.theme.gutenbergConfig?.innerBlockRestrictions ?? {};
+
 		const restriction = innerBlockRestrictions?.[ name ];
 
 		if ( ! restriction ) {
@@ -44,3 +28,19 @@ addFilter(
 		};
 	}
 );
+
+/**
+ * Resolve config for a parent block
+ */
+const resolveAllowedBlocks = ( rule = {}, defaultAllowedBlocks = [] ) => {
+	const blockSets = window?.theme.gutenbergConfig?.blockSets ?? {};
+	const base = blockSets[ rule.blockSet ] ?? [];
+	const add = rule.add ?? [];
+	const remove = rule.remove ?? [];
+
+	const merged = [ ...defaultAllowedBlocks, ...base, ...add ];
+
+	return [ ...new Set( merged ) ].filter(
+		( blockName ) => ! remove.includes( blockName )
+	);
+};
