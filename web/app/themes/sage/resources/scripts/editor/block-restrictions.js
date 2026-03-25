2bf20dcae1518547380a804278df37e3cb05619d
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-25 09:58:54 +0100

    chore: move block-restrictions logic to a package

--- web/app/themes/sage/resources/scripts/editor/block-restrictions.js
+++ web/app/themes/sage/resources/scripts/editor/block-restrictions.js
@@ -1,46 +1,9 @@
-/**
- * WordPress dependencies
- */
-import { addFilter } from '@wordpress/hooks';
+import { registerBlockRestrictions } from '@yardinternet/gutenberg-block-restrictions';
 
-/**
- * Only allow certain blocks in some blocks. See `innerBlockRestrictions` in `config/gutenberg.php` for configuration.
- */
-addFilter(
-	'blocks.registerBlockType',
-	'yard/restrict-inner-blocks',
-	( settings, name ) => {
-		const innerBlockRestrictions =
-			window?.theme?.gutenbergConfig?.innerBlockRestrictions ?? {};
+const { innerBlockRestrictions = {}, blockSets = {} } =
+	window?.theme?.gutenbergConfig || {};
 
-		const restriction = innerBlockRestrictions?.[ name ];
-
-		if ( ! restriction ) {
-			return settings;
-		}
-
-		return {
-			...settings,
-			allowedBlocks: resolveAllowedBlocks(
-				restriction,
-				settings.allowedBlocks ?? []
-			),
-		};
-	}
-);
-
-/**
- * Resolve config for a parent block
- */
-const resolveAllowedBlocks = ( rule = {}, defaultAllowedBlocks = [] ) => {
-	const blockSets = window?.theme?.gutenbergConfig?.blockSets ?? {};
-	const base = blockSets[ rule.blockSet ] ?? [];
-	const add = rule.add ?? [];
-	const remove = rule.remove ?? [];
-
-	const merged = [ ...defaultAllowedBlocks, ...base, ...add ];
-
-	return [ ...new Set( merged ) ].filter(
-		( blockName ) => ! remove.includes( blockName )
-	);
-};
+registerBlockRestrictions( {
+	innerBlockRestrictions,
+	blockSets,
+} );
