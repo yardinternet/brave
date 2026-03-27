1d29fc9a11e6f454afec259ef84a086440119c53
Simon <simon@yard.nl>
2026-03-27 15:07:51 +0100

    feat: enhance deploy script to support gemeente site setup

--- deploy.php
+++ deploy.php
@@ -13,26 +13,38 @@ require_once __DIR__ . '/vendor/sourcebroker/deployer-loader/autoload.php';
 
 new Loader();
 
-//Config
+// Config
 set('repository', 'git@github.com:yardinternet/brave');
 
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
-////////// For demo site only
+////////// For demo brave/gemeente sites only
 after('deploy:vendors', function () {
-	within('{{release_or_current_path}}', function () {
-		run('{{bin/composer}} private-packages');
-		run('{{bin/composer}} require yard/brave-scaffold --dev');
-		wp('acorn scaffold:news');
-		wp('acorn scaffold:person');
-		wp('acorn scaffold:knowledgebase');
-	});
+	if (get('labels')['site'] ?? '' === 'brave') {
+		within('{{release_or_current_path}}', function () {
+			run('{{bin/composer}} private-packages');
+			run('{{bin/composer}} require yard/brave-scaffold --dev');
+			wp('acorn scaffold:news');
+			wp('acorn scaffold:person');
+			wp('acorn scaffold:knowledgebase');
+		});
+	}
+
+	if (get('labels')['site'] ?? '' === 'gemeente') {
+		within('{{release_or_current_path}}', function () {
+			run('{{bin/composer}} gemeente-packages');
+		});
+	}
 })->desc('Scaffold default content types for Brave');
 //////////
 
 // Hosts.
 host(Host::WPACC01)
 	->setDeployPath('/data/www/accept-sites/yard/brave')
-	->setLabels(['stage' => Stage::ACCEPT]);
+	->setLabels(['stage' => Stage::ACCEPT, 'site' => 'brave']);
+
+host(Host::WPACC01)
+	->setDeployPath('/data/www/accept-sites/yard/gemeente')
+	->setLabels(['stage' => Stage::ACCEPT, 'site' => 'gemeente']);
