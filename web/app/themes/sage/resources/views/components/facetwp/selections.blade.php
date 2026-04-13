96112aab6f7f50aeb0b4d835a7d15860fbdac369
Lara van Diemen <lara@yard.nl>
2026-04-13 12:05:04 +0200

    feat: hide facetwp-selections when they are empty

--- web/app/themes/sage/resources/views/components/facetwp/selections.blade.php
+++ web/app/themes/sage/resources/views/components/facetwp/selections.blade.php
@@ -1,6 +1,7 @@
-<div {{ $attributes->merge([
-    'class' => 'flex flex-wrap items-center gap-x-2',
-]) }}>
+<div
+	{{ $attributes->merge([
+	    'class' => 'flex flex-wrap items-center gap-x-2 has-[.facetwp-selections:empty]:hidden',
+	]) }}>
 	<span class="js-brave-facetwp-filter-label text-sm">Filters:</span>
 	{!! facetwp_display('selections') !!}
 </div>
