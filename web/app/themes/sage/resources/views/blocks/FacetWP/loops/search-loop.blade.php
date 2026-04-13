993be0d49933d6ed6e6804feac4f107272dae9ff
Lara van Diemen <lara@yard.nl>
2026-04-13 12:04:28 +0200

    fix: change facetwp loops from grid to flex

--- web/app/themes/sage/resources/views/blocks/FacetWP/loops/search-loop.blade.php
+++ web/app/themes/sage/resources/views/blocks/FacetWP/loops/search-loop.blade.php
@@ -1,6 +1,6 @@
 @use(App\View\Components\Card\Enums\Direction)
 
-<div class="@xl:gap-6 grid gap-4">
+<div class="@xl:gap-6 flex flex-col gap-4">
 	@forelse ($postDataCollection as $postData)
 		<x-dynamic-card :card="$postData->postType()" class="card-large" :postData="$postData" :direction="Direction::FLUID" :label="get_post_type_object($postData->postType())?->labels->singular_name" />
 	@empty
