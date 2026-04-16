73358f682d8ac0375bbc74d2eddeefef3e54e5ce
JasperS20 <jasper.samson@kpnmail.nl>
2026-04-16 15:27:14 +0200

    feat: added stages config

---
+++ web/app/themes/sage/config/stages.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// Set the stage url's. Replace 'brave' with the correct site.
+return [
+	'development' => 'https://brave.lndo.site',
+	'staging' => 'https://brave.wpacc01.yard.nl',
+	'production' => '',
+];
