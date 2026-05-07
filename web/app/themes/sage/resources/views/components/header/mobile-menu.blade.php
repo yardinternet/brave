352e81e919d60f6f3be42c735cf59ae049cd99e0
Lara van Diemen <lara@yard.nl>
2026-05-07 11:29:15 +0200

    chore: remove unused group-aria-expanded

--- web/app/themes/sage/resources/views/components/header/mobile-menu.blade.php
+++ web/app/themes/sage/resources/views/components/header/mobile-menu.blade.php
@@ -10,13 +10,10 @@
     'label' => __('Menu', 'sage'),
 ])
 
-<x-brave::dialog.trigger :dialogId="$dialogId" class="hamburger group flex h-[46px] flex-col gap-y-2 lg:hidden">
-	<span
-		class="block h-0.5 w-8 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:w-8 group-aria-expanded:translate-y-3 group-aria-expanded:rotate-45"></span>
-	<span
-		class="block h-0.5 w-6 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:scale-0"></span>
-	<span
-		class="block h-0.5 w-4 rounded-full bg-black transition-all duration-300 ease-in-out group-aria-expanded:w-8 group-aria-expanded:-translate-y-2 group-aria-expanded:-rotate-45"></span>
+<x-brave::dialog.trigger :dialogId="$dialogId" class="hamburger flex h-[46px] flex-col gap-y-2 lg:hidden">
+	<span class="block h-0.5 w-8 rounded-full bg-black transition-all duration-300 ease-in-out"></span>
+	<span class="block h-0.5 w-6 rounded-full bg-black transition-all duration-300 ease-in-out"></span>
+	<span class="block h-0.5 w-4 rounded-full bg-black transition-all duration-300 ease-in-out"></span>
 	<span class="mt-auto text-xs leading-none">{{ $label }}</span>
 </x-brave::dialog.trigger>
 
