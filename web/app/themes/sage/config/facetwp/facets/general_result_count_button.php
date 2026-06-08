c1f8b46b7e2ff6f6cde4a1fc2f61f1dbdecab422
YvetteNikolov <yvettenikolov@gmail.com>
2026-06-08 11:30:27 +0200

    feat: add facetwp result count to mobile filters button

---
+++ web/app/themes/sage/config/facetwp/facets/general_result_count_button.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'name' => 'result_count_button',
+	'label' => 'Resultaten',
+	'type' => 'pager',
+	'pager_type' => 'counts',
+	'count_text_plural' => 'Bekijk resultaten <span class="text-xs">([total])</span>',
+	'count_text_singular' => 'Bekijk resultaat <span class="text-xs">(1)</span>',
+	'count_text_none' => 'Geen resultaten <span class="text-xs">(0)</span>',
+];
