143f4be7e68bf478ef84c7876e34bacb192f6575
Yannic van Veen <yannic.vanveen@yard.nl>
2026-09-08 10:33:17 +0200

    feat: add card block default template / move shared theme block editor styles

--- web/app/themes/sage/resources/scripts/blocks/card/edit.jsx
+++ web/app/themes/sage/resources/scripts/blocks/card/edit.jsx
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 /**
@@ -8,12 +9,28 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
  */
 import './editor-style.css';
 
+const TEMPLATE = [
+	[
+		'core/heading',
+		{
+			content: __( 'Titel van de kaart', 'sage' ),
+			level: '3',
+		},
+	],
+	[
+		'core/paragraph',
+		{
+			content: __( 'Korte tekst van ongeveer 3 regels. Cupidatat amet nostrud non elit amet cupidatat elit sit proident anim duis.', 'sage' ),
+		},
+	],
+];
+
 const Edit = () => {
 	const blockProps = useBlockProps();
 
 	return (
 		<div { ...blockProps }>
-			<InnerBlocks />
+			<InnerBlocks template={ TEMPLATE } />
 		</div>
 	);
 };
