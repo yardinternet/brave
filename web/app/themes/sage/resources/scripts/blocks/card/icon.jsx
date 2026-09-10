ee69eca3bb0763db040f4ec366538baf822da128
Yannic van Veen <yannic.vanveen@yard.nl>
2026-09-10 15:47:42 +0200

    feat: add group alert variation and style / update icons

--- web/app/themes/sage/resources/scripts/blocks/card/icon.jsx
+++ web/app/themes/sage/resources/scripts/blocks/card/icon.jsx
@@ -5,11 +5,8 @@ import { BlockIconColor } from '@yardinternet/gutenberg-components';
 
 const icon = {
 	src: (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-			<path
-				fillRule="evenodd"
-				d="M16 16h480v480H16zm16 16v448h448V32zm64 80h320v112H96zm0 176h320v112H96z"
-			/>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+			<path d="M480 512C480 529.7 465.7 544 448 544L192 544C174.3 544 160 529.7 160 512L160 128C160 110.3 174.3 96 192 96L448 96C465.7 96 480 110.3 480 128L480 512zM512 128C512 92.7 483.3 64 448 64L192 64C156.7 64 128 92.7 128 128L128 512C128 547.3 156.7 576 192 576L448 576C483.3 576 512 547.3 512 512L512 128z" />
 		</svg>
 	),
 	...BlockIconColor,
--- web/app/themes/sage/resources/scripts/blocks/section/icon.jsx
+++ web/app/themes/sage/resources/scripts/blocks/section/icon.jsx
@@ -5,10 +5,9 @@ import { BlockIconColor } from '@yardinternet/gutenberg-components';
 
 const icon = {
 	src: (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
 			<path
-				fillRule="evenodd"
-				d="M16 16h480v480H16zm16 16v448h448V32zm64 80h320v112H96zm0 176h320v112H96z"
+				d="M576 160C593.7 160 608 174.3 608 192L608 448C608 465.7 593.7 480 576 480L64 480C46.3 480 32 465.7 32 448L32 192C32 174.3 46.3 160 64 160L576 160zM64 128C28.7 128 0 156.7 0 192L0 448C0 483.3 28.7 512 64 512L576 512C611.3 512 640 483.3 640 448L640 192C640 156.7 611.3 128 576 128L64 128z"
 			/>
 		</svg>
 	),
