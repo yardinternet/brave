95d1ea903eafc01dc1dca3703cbad9fbc4501bae
Yannic van Veen <yannic.vanveen@yard.nl>
2026-09-11 14:01:14 +0200

    feat: add default template to article block

--- web/app/themes/sage/resources/scripts/blocks/article/edit.jsx
+++ web/app/themes/sage/resources/scripts/blocks/article/edit.jsx
@@ -1,20 +1,54 @@
 /**
  * WordPress dependencies
  */
-import {
-	useBlockProps,
-	useInnerBlocksProps,
-} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import './editor-style.css';
 
+const TEMPLATE = [
+	[
+		'theme/back-button',
+		{
+			align: 'none',
+			lock: {
+				remove: true,
+				move: true,
+			},
+		},
+	],
+	[
+		'core/post-title',
+		{
+			level: 1,
+			lock: {
+				remove: true,
+				move: true,
+			},
+		},
+	],
+	[
+		'core/paragraph',
+		{
+			content: __(
+				'Korte tekst van ongeveer 3 regels. Cupidatat amet nostrud non elit amet cupidatat elit sit proident anim duis.',
+				'sage'
+			),
+		},
+	],
+];
+
 const Edit = () => {
-	const innerBlocksProps = useInnerBlocksProps(useBlockProps({
+	const blockProps = useBlockProps( {
 		className: 'layout-article',
-	}) );
+	} );
+
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		template: TEMPLATE,
+	} );
 
 	return <div { ...innerBlocksProps } />;
 };
